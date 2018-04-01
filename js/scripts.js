e8a516c77ac8c1e75aed54d518a8b529f7bf47fd
Kevin te Hennepe <keviee0007@gmail.com>
2018-04-01 22:51:36 +0200

    New detail pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,47 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
         sectionSelector: '.page',
-        scrollOverflow: true
+        scrollOverflow: true,
+        responsiveWidth: 1100
 	});
 });
 
-function openNav() {
-        document.getElementById("nav").style.width = "250px";
+$('.navbar-nav a').on('click', function () {
+        $('.navbar-nav').find('li.active').removeClass('active');
+        $(this).parent('li').addClass('active');
+});
+
+var artworks = $('#artworks');
+
+var images = $(artworks).find(document.getElementsByClassName("item"));
+
+console.log('images', images);
+
+function shuffleProjects(array) {
+        var currentIndex = array.length, tempValue, randomIndex;
+
+        while (0 !== currentIndex) {
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+
+                tempValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = tempValue;
+        }
+        return array;
 }
 
-function closeNav() {
-        document.getElementById("nav").style.width = "0";
+var randomImages = shuffleProjects(images);
+
+console.log("random image: ", randomImages);
+
+function renderProjects(array) {
+        $(artworks).empty();
+
+        $.each(array, function(index, value) {
+                $(artworks).append(value);
+        });
 }
+randomImages = shuffleProjects(images);
+renderProjects(randomImages);
+
