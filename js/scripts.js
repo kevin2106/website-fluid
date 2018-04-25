7c66e04fb01a4d8344389ed9923ce72b18e934eb
Kevin te Hennepe <keviee0007@gmail.com>
2018-04-25 16:20:09 +0200

    added media query

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
         sectionSelector: '.page',
-        scrollOverflow: true,
+        scrollOverflow: false,
         responsiveWidth: 1100
 	});
 });
